8606b87faac37660b51346725d1421ed7dee407d
fupengpeng <564055710@qq.com>
2017-05-22 18:12:54 +0800

    aaa

--- app/src/main/java/com/fupengpeng/cameraapp/MainActivity.java
+++ app/src/main/java/com/fupengpeng/cameraapp/MainActivity.java
@@ -1,18 +1,25 @@
 package com.fupengpeng.cameraapp;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
     public static final int REQ_1 = 1;
+
+    private ImageView mImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mImageView = (ImageView) findViewById(R.id.image);
+
     }
     public void startCamera(View view){
         Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
@@ -25,7 +32,10 @@ public class MainActivity extends AppCompatActivity {
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK){
             if (requestCode == REQ_1){
-                
+                //返回饱含整个图片的二进制流
+                Bundle bundle = data.getExtras();
+                Bitmap bitmap = (Bitmap) bundle.get("data");
+                mImageView.setImageBitmap(bitmap);
             }
         }
     }
